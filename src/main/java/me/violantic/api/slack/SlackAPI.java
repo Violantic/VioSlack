33147b52d747b365cd5d63ceed72270f3fbea1f4
Ethan <ethan@tesseractltd.com>
2016-02-03 15:28:31 -0800

    Initial commit

---
+++ src/main/java/me/violantic/api/slack/SlackAPI.java
@@ -0,0 +1,23 @@
+package me.violantic.api.slack;
+
+import me.violantic.api.slack.api.Slack;
+
+/**
+ * @author Ethan
+ *
+ * This class just tests the use of the API.
+ */
+public class SlackAPI {
+
+    private static Slack slack;
+
+    public static void main(String[] args) {
+        init();
+        slack.send("#general", "Hello, I am Violantic, and this is testing the Slack API!");
+    }
+
+    private static void init() {
+        slack = new Slack("VioBot");
+    }
+
+}
---
+++ src/main/java/me/violantic/api/slack/api/Slack.java
@@ -0,0 +1,77 @@
+package me.violantic.api.slack.api;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import javax.net.ssl.HttpsURLConnection;
+import java.io.BufferedOutputStream;
+import java.net.URL;
+
+/**
+ * @author Ethan
+ */
+public class Slack {
+
+    public String user;
+    public String webhook_url = "https://hooks.slack.com/services/<id goes here>";
+    public String avatar_url = "https://minotar.net/helm/Violantic/150.png";
+
+    public Slack(String user) {
+        this.user = user;
+    }
+
+    public String getUser() {
+        return this.user;
+    }
+
+    public void send(String channel, String message) {
+        final JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("channel", channel);
+            jsonObject.put("text", message);
+            jsonObject.put("username", getUser());
+            jsonObject.put("icon_url", avatar_url);
+
+            new Thread() {
+                @Override
+                public void run() {
+                    try {
+                        HttpsURLConnection huc = (HttpsURLConnection) (new URL(webhook_url)).openConnection();
+                        huc.setRequestMethod("POST");
+                        huc.setDoOutput(true);
+
+                        try {
+                            BufferedOutputStream bufOut = new BufferedOutputStream(huc.getOutputStream());
+
+                            try {
+                                String jsonStr = "payload=" + jsonObject.toString();
+                                bufOut.write(jsonStr.getBytes("utf8"));
+                                bufOut.flush();
+                                bufOut.close();
+                            } finally {
+                                if (bufOut != null) {
+                                    bufOut.close();
+                                }
+                            }
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+
+                        huc.getResponseCode();
+                        huc.disconnect();
+                    } catch (Exception e1) {
+                        e1.printStackTrace();
+                    }
+
+                }
+            }.start();
+        } catch (JSONException e2) {
+            e2.printStackTrace();
+        }
+    }
+
+    public void sendDirect(String user, String message) {
+        send("@" + user, message);
+    }
+
+}
